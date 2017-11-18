45c0c7df7c4f8d913b806b2e1b6ed1fa08b168a6
Viktor Persson <viktor.persson@arcsin.se>
2017-11-18 21:43:04 +0100

    Added module, tests.

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/iis-bardate-domains-parser');
---
+++ test/iis-bardate-domains-parser.test.js
@@ -0,0 +1,74 @@
+var axios = require('axios');
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var iisParser = require('./../lib/iis-bardate-domains-parser');
+var readFileSync = require('fs').readFileSync;
+
+describe('IIS bardate domains parser', function() {
+  describe('parse', function() {
+
+    describe('live text file', function() {
+      it('should handle success', function() {
+        sinon.spy(axios, 'get');
+
+        return iisParser
+          .parseAsync()
+          .then(function(parsedDomains) {
+            sinon.assert.calledWith(axios.get, 'https://www.iis.se/data/bardate_domains.txt');
+            expect(parsedDomains.length).to.be.greaterThan(1000);
+            axios.get.restore();
+          });
+      });
+    });
+
+    describe('mocked text file', function() {
+      beforeEach(function() {
+        sinon.stub(axios, 'get');
+      });
+
+      afterEach(function() {
+        axios.get.restore();
+      });
+
+      it('should handle success', function() {
+        var buffer = readFileSync(__dirname + '\\bardate_domains.txt');
+
+        var responseMock = {
+          status: 200,
+          data: buffer
+        };
+
+        axios.get.resolves(responseMock);
+
+        return iisParser
+          .parseAsync()
+          .then(function(parsedDomains) {
+            sinon.assert.calledWith(axios.get, 'https://www.iis.se/data/bardate_domains.txt');
+
+            var expectedDomains = [
+              {name: '002.se', date: '2017-12-03'},
+              {name: '123-456.se', date: '2018-01-23'},
+              {name: 'abc-123.se', date: '2018-01-23'},
+              {name: 'abc-def.se', date: '2018-01-23'},
+              {name: 'abc.se', date: '2018-01-23'},
+              {name: 'abc123.se', date: '2018-01-23'}
+            ];
+
+            expect(parsedDomains).to.eql(expectedDomains);
+          });
+      });
+
+      it('should handle failure', function() {
+        axios.get.rejects(new Error('URL not found'));
+
+        return iisParser
+          .parseAsync()
+          .catch(function(error) {
+            sinon.assert.calledWith(axios.get, 'https://www.iis.se/data/bardate_domains.txt');
+
+            expect(error.toString()).to.equal('Error: URL not found');
+          });
+      });
+    });
+  });
+});
